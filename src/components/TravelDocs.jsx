7ae069385f8badf68cd2b2902d353ef3b3a9c07a
Sam Tilo <tilosamantha@gmail.com>
2021-03-08 22:14:11 -0700

    created Travel Docs page outline

---
+++ src/components/TravelDocs.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../App.css';
+
+
+function TravelDocs() {
+  return (
+    <div className="focus">
+      <h1 className="title">Important Documents****</h1>
+      <p>This will include all the necessary documents needed for traveling to your destination, including:</p>
+      <p>Embassy information</p>
+      <p>National Emergency contact information (i.e. 911)</p>
+      <p>Passport</p>
+      <p>Vaccinations</p>
+    </div>
+  )
+};
+
+
+export default TravelDocs;
